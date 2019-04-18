390a2c418795b20cf7231d56dfa454efbbbc4450
ariel hadad <arieell25@gmail.com>
2019-04-18 15:23:48 +0300

    practice

---
+++ exercises/ex6/includes/main.js
@@ -0,0 +1,15 @@
+$("document").ready(function(){
+// var section= $('section')
+alert('hello world');
+});
+
+// $("decument").ready(function() {
+//     console.log("hello world using JQuery!");
+//     });
+
+
+// $("target").click(function() {
+// alert("handler for.click() called.");
+// });
+
+// $("buttom").click(funct)
