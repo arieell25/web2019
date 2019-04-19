564daf165fefb5eeaf189bf60063eb49f35205da
ariel hadad <arieell25@gmail.com>
2019-04-19 12:10:39 +0300

    one step

--- exercises/ex6/includes/main.js
+++ exercises/ex6/includes/main.js
@@ -1,7 +1,24 @@
 $("document").ready(function(){
-// var section= $('section')
-alert('hello world');
-});
+    var img=$('img')
+    var buttom=$(
+
+    )
+$("buttom").click(function(){
+    alert("handle for. click() called.")
+
+
+<form>
+<input type="button" value="Input Button">
+<input type="checkbox">
+<input type="checkbox">
+<input type="hidden">
+<input type="button" value="Output Button">
+<select>
+<option>Option</option>
+</select>
+</form>
+var input = $( "form input:checkbox" );
+
 
 // $("decument").ready(function() {
 //     console.log("hello world using JQuery!");
