20d10b67ed2d04a2ca1b54807672199fc290d658
ariel hadad <arieell25@gmail.com>
2019-05-13 15:15:49 +0300

    finish

--- exercises/ex3/includes/main.js
+++ exercises/ex3/includes/main.js
@@ -1,81 +1,90 @@
-var main = document.getElementById('main');
-var nav = document.getElementById('nav');
-var boxshow = document.getElementById('js-boxshow');
-var aside = document.getElementById('aside');
+var boxes = document.getElementById('creatBoxsJS');
 
 var height = 80;
 var width = 80;
-var startHeight=995;
-var widthAllBox=0;
-var charLetters= ['A','R','I','E','L'];
-var choice=[];
+var startHeight = 995;
+var startWidth = 0;
+var charLetters = ['A','R','I','E','L'];
+var choice = [];
 
-var settBoxes= function(){
-    for(var i=0; i < 3; i++){
+var settBoxes= function()
+{
+
+    for(var i = 0; i < 3; i++)
+    {
         box = document.createElement('DIV');
         box.style.height = height+'px';
         box.style.width = width+'px';
         box.className ='box';
         var random = Math.floor(Math.random()*charLetters.length);
-        box.ineerHTML= '<p style="margin-top: '+height*0.25+'px; font-size: '+height*0.35+'px;">'+charLetters[random]+'</p>';
+        box.innerHTML= '<p style="margin-top: '+height*0.25+'px; font-size: '+height*0.35+'px;">'+charLetters[random]+'</p>';
         height = height + 20;
         width = width + 20;
         widthAllBox = width + 128;
 
-        box.addEventListener('click', function(){
+        box.addEventListener('click', function()
+        {
             if(choice.length >= 2){
-                clearItems();
+                clearLetters();
         }
-
-        if( !this.className.includes('show')){
-                if(!this.className.includes('correct')){
+        
+        if( !this.className.includes('show'))
+        {
+            if(!this.className.includes('correct'))
+            {
                     this.className = this.className + 'show';
                     choice.push(this);   
-                    if(choice.length == 2){
+                    if(choice.length == 2)
                         checkPlayer();
-                    }
+                
                 }
         }
-        else{
-            clearItems();
-            }
+        else
+            clearLetters();
         });
-        boxshow.appendChild(box);
-}
+        boxes.appendChild(box);
+    }
 
-var memoryGame = function(){
-        var str1 = choice[0].innerHTML;
-        var str2 = choice[1].innerHTML;
-        var index=str1.indexOf('">');
-        var slashIndex=str1.indexOf('</p');
-        var fstr1=str1.substring(index+2,slashIndex);
-        index=str2.indexOf('">');
-        slashIndex=str2.indexOf('</p');
-        var fstr2= str2.substring(index+2,slashIndex);
+var memoryGame = function()
+{
+        var letter1 = choice[0].innerHTML;
+        var letter2 = choice[1].innerHTML;
+        
+        var index=letter1.indexOf('">');
+        var cutPar=letter1.indexOf('</p');
+        var str1=letter1.substring(index+2,cutPar);
+        index=letter2.indexOf('">');
+        cutPar=letter2.indexOf('</p');
+        var str2=letter2.substring(index+2,cutPar);
 
-        if(fstr1 == fstr2)
+        if(str1 == str2)
             return true;
         return false;
-    };
+};
 
-var clearItems=function(){
-    for(var i=0; i<choice.length; i++){
-        choice[i].className = choice[i].className.replace('show','');
-    }
-    choice = [];
-}
-
-var checkPlayer = function(){
+var checkPlayer = function()
+{
     if(memoryGame()){
-        for (var j = 0; j < choice.length; j++){
-        choice[j].className = choice[j].className.replace('show','correct');
+        for (var i = 0; i < choice.length; i++)
+        {
+        choice[i].className = choice[i].className.replace('show','correct');  //check if the letters are the same
         }
     }
-    else{
-        setTimeout(function(){
-            clearItems();
+    else
+    {
+        setTimeout(function()
+        { 
+            clearLetters();   //timer 0.5sec 
         }, 500);
     }
 
+}
+
+var clearLetters = function()
+{
+    for( var i=0; i<choice.length; i++){
+        choice[i].className = choice[i].className.replace('show','');
+    }
+    choice = [];
 }
 }
