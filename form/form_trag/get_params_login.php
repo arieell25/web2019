fa77b5202789a037e5dd7f0f0c2b15961aa8b9f6
ariel hadad <arieell25@gmail.com>
2019-05-14 21:58:26 +0300

    finishh

---
+++ form/form_trag/get_params_login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>forms</title>
+    </head>
+    <body>
+        <section>
+            Welcome
+            <?php
+                $un = $_GET["reg_un"];
+                $pw = $_GET["reg_pass"];
+
+                if($un == )
