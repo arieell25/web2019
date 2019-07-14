2c180a6462857bef7e160cab11ddd241a1d78161
Ariel <48453004+arieell25@users.noreply.github.com>
2019-07-14 19:47:10 +0300

    up

--- exercises/SCM/get_params.php
+++ exercises/SCM/get_params.php
@@ -1,54 +1,32 @@
-<?php
-//get data from our from and insert to the table
-include 'db.php';
-$uname = $_GET["name"];
-$ucode = $_GET["code"];
-if ($_GET["kind"] == "aerobic") {
-    $unum = 1;
-} else {
-    $unum = 0;
-}
-$upro = $_GET["provider"];
-$udate = $_GET["date"];
-$ucomm = $_GET["comments"];
-$uimg = $_GET["img"];
-$query = "INSERT INTO tb_facilities_210 (ID, name, provider,date,comment,kind,img)
-            VALUES ('$ucode', '$uname', '$upro','$udate','$ucomm','$unum','$uimg')";
-$result = mysqli_query($connection, $query);
-if (!$result) {
-    die("DB query failed.");
-}
-?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>SCM</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="includes/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body id="detailsSaveLayout">
     <div id="wrapper">
         <header>
             <section class="loginLine">
-                <a href="informationUser.php"><?php session_start();
-                            echo  $_SESSION["user_name"]; ?></a>
+                <a href="#">Efrat</a>
                 <a href="#">Support</a>
             </section>
-            <a class="logo1" href="index_admin.php"></a>
-            <a class="logo2" href="index_admin.php"></a>
+            <a class="logo1" href="#"></a>
+            <a class="logo2" href="#"></a>
         </header>
         <nav>
             <ul class="nav nav-tabs">
                 <li class="nav-item">
-                    <a class="nav-link" href="index_admin.php">Home</a>
+                    <a class="nav-link" href="../index.html">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Report</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="facilitysLayout.php">Facilities</a>
+                    <a class="nav-link active" href="../facilitysLayout.html">Facilities</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Subscribers</a>
@@ -56,37 +34,45 @@ if (!$result) {
             </ul>
         </nav>
         <ul class="breadcrumb">
-            <li><a href="index_admin.php">Home</a></li>
-            <li><a href="facilitysLayout.php">Facilities</a></li>
-            <li><a href="formLayout.php">Add a facility</a></li>
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="../facilitysLayout.html">Facilities</a></li>
+            <li><a href="../formLayout.html">Add a facility</a></li>
             <li class="currentPage">Details saved</li>
         </ul>
         <main>
-            <?php
-                 echo "<div class='card mb-3' style='max-width: 540px;'>
-                <div class='row no-gutters'>
-                    <div class='col-md-4'>
-                        <img src='" . $uimg . "' class='card-img' alt=''>
-                    </div>
-                    <div class='col-md-8'>
-                        <div class='card-body'>
-                        <ul class='list-group list-group-flush'>
-                        <li class='list-group-item'>" . $ucode . "</li>
-                        <li class='list-group-item'>" . $uname . "</li>
-                        <li class='list-group-item'>" . $upro . "</li>
-                        <li class='list-group-item'>" . $udate . "</li>
-                        <li class='list-group-item'>" . $ucomm . "</li>
-                    </ul>
-                      
-                    </div>
-                </div>
-            </div>";
-            ?>
+        <?php
+        $uname=$_GET["name"];
+        $ucode=$_GET["code"];
+        $unum=$_GET["number"];
+        $upro=$_GET["provider"];
+        $udate=$_GET["date"];
+        $ucomm=$_GET["comments"];
+            echo "<table class=table>
+                <thead>
+                    <tr>
+                        <th scope=col></th>
+                        <th scope=col>Name</th>
+                        <th scope=col>Code</th>
+                        <th scope=col>Number</th>
+                        <th scope=col>Provider</th>
+                        <th scope=col>Date</th>
+                        <th scope=col>Comments</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope=row>1</th>
+                        <td>". $uname ."</td>
+                        <td>". $ucode ."</td>
+                        <td>". $unum ."</td>
+                        <td>". $upro ."</td>
+                        <td>". $udate ."</td>
+                        <td>". $ucomm ."</td>
+                    </tr>
+                </tbody>
+            </table>";
+        ?>
         </main>
-    </div>
+    </div>   
 </body>
 </html>
-<?php
-//close DB connection
-mysqli_close($connection);
-?>
